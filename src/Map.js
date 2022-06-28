de4710238824ddcf35b8d5d701ddb0b7abbe2ef8
Rob Thomas <r.thomas@smt.com>
2022-06-27 22:48:13 -0400

    Add markers for Favorites that have a latitude/longitude

--- src/Map.js
+++ src/Map.js
@@ -1,38 +1,41 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+
+import { useContext } from "react";
+import DataContext from "./context/DataContext";
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-     iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-     iconUrl: require('leaflet/dist/images/marker-icon.png'),
-     shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+    iconUrl: require("leaflet/dist/images/marker-icon.png"),
+    shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
 
 const Map = () => {
-    const position = [44.98106, -93.174351]
+    const center = [44.98106, -93.174351];
+    const zoom = 20;
+
+    const { favorites } = useContext(DataContext);
+
+    const favs = favorites.filter((favorite) =>
+        favorite.hasOwnProperty("latitude")
+    );
+
     return (
         <main className="Map">
-            <MapContainer center={position} zoom={20} scrollWheelZoom={false}>
+            <MapContainer center={center} zoom={zoom} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[44.9856217722, -93.1690153538]}>
-                    <Popup>
-                        <h4>Giggles Campfire Grill</h4>
-                        <ul>
-                            <li>Bear Republic Racer 5 IPA</li>
-                        </ul>
-                    </Popup>
-                </Marker>
-                <Marker position={[44.9872933995, -93.1701421093]}>
-                    <Popup>
-                        <h4>The Hangar</h4>
-                        <ul>
-                            <li>Bent Paddle Kanu</li>
-                        </ul>
-                    </Popup>
-                </Marker>
+
+                {favs.map((f) => (
+                    <Marker key={f.id} position={[f.latitude, f.longitude]}>
+                        <Popup>
+                            <h4>{f.name}</h4>
+                        </Popup>
+                    </Marker>
+                ))}
             </MapContainer>
         </main>
     );
